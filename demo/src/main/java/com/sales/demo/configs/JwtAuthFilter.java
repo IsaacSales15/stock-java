3a04a296eac4b71cedfe6aaffa658b1d40da8eff
Isaac Sales <isaacrodrigues1516@gmail.com>
2025-05-02 21:17:22 -0300

    auth middleware

--- demo/src/main/java/com/sales/demo/configs/JwtAuthFilter.java
+++ demo/src/main/java/com/sales/demo/configs/JwtAuthFilter.java
@@ -1,5 +1,38 @@
 package com.sales.demo.configs;
 
-public class JwtAuthFilter {
-    
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            try {
+                Long userId = JwtSecurity.extractUserId(token);
+
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(userId, null, null);
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (Exception e) {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token inválido ou expirado.");
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
